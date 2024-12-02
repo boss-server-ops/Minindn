0b2e48ce05173a3da1a66b8480a5e3ed3d645725
dd <1312136480@qq.com>
2024-12-02 09:14:30 +0000

    ignore executable files

--- examples/test_consumer_producer.py
+++ examples/test_consumer_producer.py
@@ -1,3 +1,6 @@
+# written by Yixiang Zhu
+
+
 import os
 from time import sleep
 from mininet.log import setLogLevel, info
@@ -23,36 +26,29 @@ if __name__ == '__main__':
     nfds = AppManager(ndn, ndn.net.hosts, Nfd)
     info('Starting NLSR on nodes\n')
     nlsrs = AppManager(ndn, ndn.net.hosts, Nlsr)
-    sleep(90)  # 等待路由收敛
+    sleep(60)  #wait for routing convergence
 
-    # 获取节点a和b
     node_a = ndn.net.get('a')
-    node_b = ndn.net.get('c')
+    node_b = ndn.net.get('b')
 
     # 获取consumer和producer的绝对路径
-    consumer_path = os.path.abspath('./apps/consumer')
-    producer_path = os.path.abspath('./apps/producer')
-    print(f'Consumer absolute path: {consumer_path}')
-    print(f'Producer absolute path: {producer_path}')
+    consumer_path = os.path.abspath('./apps/consumer_test')
+    producer_path = os.path.abspath('./apps/producer_test')
 
     # 在节点b上启动producer程序
     info('Starting Producer on node b\n')
     producer = Application(node_b)
-    producer.start(producer_path, 'producer.log')  # 使用绝对路径
-    node_b.cmd('ps aux | grep producer')  # 打印进程信息
+    producer.start(producer_path, 'producertest.log')  
 
     producerPrefix = "/example/testApp"
     node_b.cmd('nlsrc advertise {}'.format(producerPrefix))  # 在节点b上广告前缀
     sleep(5)  # 等待路由信息传播
-
-    # 检查节点b的NLSR日志文件
-    info('Checking NLSR log on node b\n')
-    print(node_b.cmd('cat /tmp/minindn/b/log/nlsr.log'))
+    # sleep(5)
 
     # 在节点a上启动consumer程序
     info('Starting Consumer on node a\n')
     consumer = Application(node_a)
-    consumer.start(consumer_path, 'consumer.log')  # 使用绝对路径
+    consumer.start(consumer_path, 'consumertest.log')  # 使用绝对路径
     node_a.cmd('ps aux | grep consumer')  # 打印进程信息
 
     MiniNDNCLI(ndn.net)
