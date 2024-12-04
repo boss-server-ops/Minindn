5590cf83f79d1e186a4acf9ca1e7f8d4bc2a1728
boss-server-ops <1312136480@qq.com>
2024-12-04 08:07:38 +0000

    port producer class

---
+++ apps/ndn-producer.cpp
@@ -0,0 +1,102 @@
+#include "ndn-producer.hpp"
+
+#include "ModelData.hpp" // 假设你有一个 ModelData 类和 serializeModelData 函数
+
+Producer::Producer(const std::string &prefix)
+    : m_virtualPayloadSize(1024),
+      m_freshness(ndn::time::milliseconds(1000)),
+      m_prefixnum(0),
+      m_prefix(prefix)
+{
+    // 初始化 spdlog
+    m_logger = spdlog::basic_logger_mt("producer_logger", "logs/producer.log");
+    spdlog::set_default_logger(m_logger);
+    spdlog::set_level(spdlog::level::info); // 设置日志级别
+    spdlog::flush_on(spdlog::level::info);  // 每条 info 级别及以上的日志后刷新
+
+    spdlog::info("Producer initialized");
+}
+
+Producer::Producer()
+    : m_virtualPayloadSize(1024),
+      m_freshness(ndn::time::milliseconds(1000)),
+      m_prefixnum(0)
+{
+    // 初始化 spdlog
+    m_logger = spdlog::basic_logger_mt("producer_logger", "logs/producer.log");
+    spdlog::set_default_logger(m_logger);
+    spdlog::set_level(spdlog::level::info); // 设置日志级别
+    spdlog::flush_on(spdlog::level::warn);  // 每条 warn 级别及以上的日志后刷新
+
+    spdlog::info("Producer initialized");
+}
+
+void Producer::OnInterest(const ndn::InterestFilter &filter, const ndn::Interest &interest)
+{
+    spdlog::info("Received Interest: {}", interest.getName().toUri());
+    App::OnInterest(filter, interest);
+
+    if (!m_active)
+        return;
+    // create Data packet
+    auto data = std::make_shared<ndn::Data>(interest.getName());
+    data->setFreshnessPeriod(m_freshness);
+
+    // generate new data content
+    ModelData modelData;
+    std::default_random_engine generator(std::random_device{}());    // reate random generator
+    std::uniform_real_distribution<float> distribution(0.0f, 10.0f); // define range (0.0, 10.0)
+    modelData.parameters.clear();
+    for (int i = 0; i < 300; ++i)
+    {
+        modelData.parameters.push_back(distribution(generator));
+    }
+
+    std::vector<uint8_t> buffer;
+    serializeModelData(modelData, buffer); // 序列化数据包
+    auto bufferPtr = std::make_shared<ndn::Buffer>(buffer.data(), buffer.data() + buffer.size());
+    ndn::Block contentBlock(ndn::tlv::Content, bufferPtr);
+    data->setContent(contentBlock);
+
+    // use KeyChain to sign Data packet
+    m_keyChain.sign(*data);
+
+    // send Data packet
+    m_face->put(*data);
+}
+
+void Producer::StartApplication()
+{
+
+    App::StartApplication();
+    spdlog::info("Producer application started");
+    m_face->setInterestFilter(m_prefix.toUri(),
+                              std::bind(&Producer::OnInterest, this, std::placeholders::_1, std::placeholders::_2),
+                              std::bind(&App::OnRegisterSuccess, this, std::placeholders::_1),
+                              std::bind(&App::OnRegisterFailure, this, std::placeholders::_1, std::placeholders::_2));
+    m_face->processEvents();
+    spdlog::info("Producer finished processing events");
+}
+
+void Producer::StopApplication()
+{
+    App::StopApplication();
+    spdlog::info("Producer application stopped");
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        spdlog::error("Usage: {} <prefix>", argv[0]);
+        return 1;
+    }
+
+    // 创建 Producer 对象
+    Producer producer(argv[1]);
+
+    // 启动应用程序
+    producer.StartApplication();
+
+    return 0;
+}
---
+++ apps/ndn-producer.hpp
@@ -0,0 +1,60 @@
+#ifndef NDN_PRODUCER_H
+#define NDN_PRODUCER_H
+
+#include <ndn-cxx/face.hpp>
+#include <ndn-cxx/security/key-chain.hpp>
+#include <ndn-cxx/encoding/block.hpp>
+#include <ndn-cxx/encoding/tlv.hpp>
+#include <spdlog/spdlog.h>
+#include <random>
+#include <set>
+#include <map>
+#include <memory>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "ndn-app.hpp"
+
+class Producer : public App
+{
+public:
+    /**
+     * @brief Constructor
+     * @param prefix The prefix for which the producer has the data
+     */
+    Producer(const std::string &prefix);
+
+    /**
+     * @brief Constructor
+     */
+    Producer();
+
+    /**
+     * @brief Method that will be called every time new Interest arrives
+     * @param interest The received Interest packet
+     */
+    void OnInterest(const ndn::InterestFilter &filter, const ndn::Interest &interest) override;
+
+    /**
+     * @brief Called at time specified by Start
+     */
+    void StartApplication() override;
+
+    /**
+     * @brief Called at time specified by Stop
+     */
+    void StopApplication() override;
+
+private:
+    ndn::Name m_prefix;
+    ndn::Name m_postfix;
+    uint32_t m_virtualPayloadSize;
+    ndn::time::milliseconds m_freshness; // valid time
+
+    // uint32_t m_signature;
+    // ndn::Name m_keyLocator;
+    ndn::KeyChain m_keyChain;
+    uint32_t m_prefixnum; // customized
+};
+
+#endif // NDN_PRODUCER_H
