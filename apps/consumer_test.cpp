374ebf6323c026527a2e4b3c0789b28ab620612d
boss-server-ops <1312136480@qq.com>
2025-01-30 05:44:31 +0000

    before changing into time x label

--- apps/consumer_test.cpp
+++ apps/consumer_test.cpp
@@ -1,114 +1,3 @@
-// #include <iostream>
-// #include <ndn-cxx/face.hpp>
-// #include <ndn-cxx/security/key-chain.hpp>
-// #include <spdlog/spdlog.h>
-// #include <spdlog/sinks/basic_file_sink.h>
-// #include <chrono>
-// #include <fstream>
-
-// class ConsumerTest
-// {
-// public:
-//     ConsumerTest(const std::string &prefix) : m_prefix(prefix), m_firstRequest(true)
-//     {
-//         // 初始化 spdlog
-//         auto logger = spdlog::basic_logger_mt("consumer_logger", "logs/consumer.log");
-//         spdlog::set_default_logger(logger);
-//         spdlog::set_level(spdlog::level::info); // 设置日志级别
-//         spdlog::flush_on(spdlog::level::info);  // 每条日志后刷新
-
-//         spdlog::info("ConsumerTest initialized");
-//     }
-
-//     void run()
-//     {
-//         for (int i = 0; i < 20; ++i)
-//         {
-//             // 创建一个 Interest 数据包,名称格式 /pro0/data/seq/<i>
-//             ndn::Name interestName = m_prefix;
-//             interestName.append("seq").append(std::to_string(i));
-//             ndn::Interest interest(interestName);
-//             interest.setCanBePrefix(false);
-//             interest.setInterestLifetime(ndn::time::seconds(30));
-
-//             spdlog::info("Sending Interest: {}", interest.getName().toUri());
-
-//             // 记录开始时间
-//             m_startTime = std::chrono::high_resolution_clock::now();
-
-//             // 发送 Interest 数据包
-//             m_face.expressInterest(interest,
-//                                    bind(&ConsumerTest::onData, this, _1, _2),
-//                                    bind(&ConsumerTest::onNack, this, _1, _2),
-//                                    bind(&ConsumerTest::onTimeout, this, _1));
-//             // 处理事件循环
-//             m_face.processEvents();
-//         }
-//     }
-
-// private:
-//     void onData(const ndn::Interest &interest, const ndn::Data &data)
-//     {
-//         // 记录结束时间
-//         auto endTime = std::chrono::high_resolution_clock::now();
-//         std::chrono::duration<double> rtt = endTime - m_startTime;
-
-//         spdlog::info("Received Data: {}", data.getName().toUri());
-//         spdlog::info("RTT: {} seconds", rtt.count());
-
-//         // 保存 RTT 到文件
-//         std::ofstream rtt_file;
-//         if (m_firstRequest)
-//         {
-//             rtt_file.open("../results/rtt_128MB.txt", std::ios::out); // 清空文件内容
-//             m_firstRequest = false;
-//         }
-//         else
-//         {
-//             rtt_file.open("../results/rtt_128MB.txt", std::ios::app); // 追加内容
-//         }
-
-//         if (rtt_file.is_open())
-//         {
-//             rtt_file << "RTT: " << rtt.count() << " seconds\n";
-//             rtt_file.close();
-//         }
-//         else
-//         {
-//             spdlog::error("Failed to open RTT file!");
-//         }
-//     }
-
-//     void onNack(const ndn::Interest &interest, const ndn::lp::Nack &nack)
-//     {
-//         spdlog::error("Received Nack for Interest: {} ({})", interest.getName().toUri(), static_cast<int>(nack.getReason()));
-//     }
-
-//     void onTimeout(const ndn::Interest &interest)
-//     {
-//         spdlog::error("Interest timeout for: {}", interest.getName().toUri());
-//     }
-
-// private:
-//     ndn::Face m_face;
-//     ndn::KeyChain m_keyChain;
-//     ndn::Name m_prefix;
-//     std::chrono::high_resolution_clock::time_point m_startTime;
-//     bool m_firstRequest;
-// };
-
-// int main(int argc, char **argv)
-// {
-//     if (argc != 2)
-//     {
-//         std::cerr << "Usage: " << argv[0] << " <prefix>" << std::endl;
-//         return 1;
-//     }
-//     ConsumerTest consumer(argv[1]);
-//     consumer.run();
-//     return 0;
-// }
-
 #include <iostream>
 #include <ndn-cxx/face.hpp>
 #include <ndn-cxx/security/key-chain.hpp>
@@ -118,6 +7,8 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <chrono>
 #include <fstream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 
 class ConsumerTest
 {
@@ -132,11 +23,29 @@ public:
         spdlog::flush_on(spdlog::level::info);  // 每条日志后刷新
 
         spdlog::info("ConsumerTest initialized");
+
+        // 读取数据大小配置
+        boost::property_tree::ptree pt;
+        try
+        {
+            boost::property_tree::ini_parser::read_ini("../experiments/experiment.ini", pt);
+            m_dataSize = pt.get<size_t>("Datasize.datasize");
+            spdlog::info("Data size set to {} bytes", m_dataSize);
+        }
+        catch (const boost::property_tree::ptree_error &e)
+        {
+            spdlog::error("Failed to read datasize from configuration file: {}", e.what());
+            throw;
+        }
+
+        // 生成文件名
+        m_rttFileName = generateFileName(m_dataSize);
+        spdlog::info("RTT file name set to {}", m_rttFileName);
     }
 
     void run()
     {
-        for (int i = 0; i < 20; ++i)
+        for (int i = 0; i < 10; ++i)
         {
             ndn::Name interestName = m_prefix;
             interestName.append("version").append(std::to_string(i));
@@ -148,8 +57,8 @@ public:
 
             // 设置 SegmentFetcher 选项
             ndn::SegmentFetcher::Options options;
-            options.interestLifetime = ndn::time::milliseconds(150000); // 15 秒
-            options.maxTimeout = ndn::time::milliseconds(120000);       // 120 秒
+            options.interestLifetime = ndn::time::milliseconds(10000); // 10 秒
+            options.maxTimeout = ndn::time::milliseconds(120000);      // 120 秒
 
             // 使用 SegmentFetcher 获取数据
             auto fetcher = ndn::SegmentFetcher::start(m_face, ndn::Interest(interestName), m_validator, options);
@@ -179,12 +88,12 @@ private:
         std::ofstream rtt_file;
         if (m_firstRequest)
         {
-            rtt_file.open("../results/rtt_128MB.txt", std::ios::out); // 清空文件
+            rtt_file.open(m_rttFileName, std::ios::out); // 清空文件
             m_firstRequest = false;
         }
         else
         {
-            rtt_file.open("../results/rtt_128MB.txt", std::ios::app); // 追加内容
+            rtt_file.open(m_rttFileName, std::ios::app); // 追加内容
         }
 
         if (rtt_file.is_open())
@@ -198,6 +107,30 @@ private:
         }
     }
 
+    std::string generateFileName(size_t dataSize)
+    {
+        std::ostringstream oss;
+        oss << "../results/rtt_";
+        if (dataSize >= 1024 * 1024 * 1024)
+        {
+            oss << dataSize / (1024 * 1024 * 1024) << "GB";
+        }
+        else if (dataSize >= 1024 * 1024)
+        {
+            oss << dataSize / (1024 * 1024) << "MB";
+        }
+        else if (dataSize >= 1024)
+        {
+            oss << dataSize / 1024 << "KB";
+        }
+        else
+        {
+            oss << dataSize << "B";
+        }
+        oss << ".txt";
+        return oss.str();
+    }
+
 private:
     ndn::Face m_face;
     ndn::KeyChain m_keyChain;
@@ -205,6 +138,8 @@ private:
     ndn::security::ValidatorNull m_validator; // 使用 ValidatorNull 进行简单验证
     std::chrono::high_resolution_clock::time_point m_startTime;
     bool m_firstRequest;
+    size_t m_dataSize;
+    std::string m_rttFileName;
 };
 
 int main(int argc, char **argv)
@@ -214,7 +149,15 @@ int main(int argc, char **argv)
         std::cerr << "Usage: " << argv[0] << " <prefix>" << std::endl;
         return 1;
     }
-    ConsumerTest consumer(argv[1]);
-    consumer.run();
+    try
+    {
+        ConsumerTest consumer(argv[1]);
+        consumer.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
